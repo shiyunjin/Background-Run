870767817171fce28c8c7bc8aad1a3ef4b7de447
shiyunjin <admin@shiyunjin.cn>
2025-11-17 17:24:44 +0800

    sync: translations

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.shiyunjin.backgroundrun.opencode"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 30
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+}
